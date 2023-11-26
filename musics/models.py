ecc211f48c72109198d1d94ae0788cd3a9de6990
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-11-26 12:20:34 +0200

    setup alembic

--- musics/models.py
+++ musics/models.py
@@ -1,3 +1,59 @@
-from django.db import models
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
 
-# Create your models here.
+from musicApp.settings import Base
+
+
+class Album(Base):
+    __tablename__ = 'albums'
+
+    id = Column(
+        Integer,
+        primary_key=True,
+    )
+
+    album_name = Column(
+        String(30),
+        nullable=False,
+    )
+
+    image_url = Column(
+        String(250),
+        nullable=False,
+    )
+
+    price = Column(
+        Float,
+        nullable=False,
+    )
+
+    songs = relationship(
+        'Song',
+        back_populates='album',
+        cascade='all, delete-orphan',
+    )
+
+
+class Song(Base):
+    __tablename__ = 'songs'
+
+    id = Column(
+        Integer,
+        primary_key=True,
+    )
+
+    song_name = Column(
+        String(200),
+        nullable=False,
+    )
+
+    album_id = Column(
+        Integer,
+        ForeignKey('albums.id'),
+        nullable=False,
+    )
+
+    album = relationship(
+        'Album',
+        back_populates='songs'
+    )
