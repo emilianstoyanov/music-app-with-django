9912a074416bd668d9ebf4d4df6ce28b60247d43
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-11-28 12:22:24 +0200

    updated album details

--- musics/urls.py
+++ musics/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('song/', include([
         path('create/', views.create_song, name='create song'),
         path('play/<int:album_id>/<int:song_id>/', views.play_song, name='play song'),
+        path('serve_song/<int:album_id>/<int:song_id>/', views.serve_song, name='serve_song'),
 
     ])),
 ]
--- musics/views.py
+++ musics/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 from common.session_decorator import session_decorator
@@ -120,6 +121,7 @@ def create_song(request):
             new_song = Song(
                 song_name=form.cleaned_data['song_name'],
                 album_id=form.cleaned_data['album'],
+                music_file_data=request.FILES['music_file_data'].read()
             )
 
             session.add(new_song)
@@ -145,7 +147,22 @@ def play_song(request, album_id, song_id):
     context = {
         'album': album,
         'song': song,
-
     }
 
     return render(request, 'songs/music-player.html', context)
+
+
+@session_decorator(session)
+def serve_song(request, album_id, song_id):
+    song = session.query(Song).filter_by(id=song_id, album_id=album_id).first()
+
+    album = session.query(Album).filter_by(id=album_id).first()
+
+    if song:
+        response = HttpResponse(song.music_file_data, content_type='audio.mpeg')
+        response['Content-Disposition'] = 'inline; filename="{}"'.format(song.song_name)
+
+        return response
+
+    else:
+        return HttpResponse('Song not found', status=404)
